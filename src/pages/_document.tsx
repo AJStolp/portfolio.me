d9a450671bdee2c666ba5a8218d8ffc8970d71a7
Anthony Stolp <anthony.stolp@gmail.com>
2023-03-01 16:38:28 -0500

    updated anlaytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,20 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { GA_TRACKING_ID, initGA } from "@/utils/ga";
+import { useEffect } from "react";
 
 export default function Document() {
+  useEffect(() => {
+    initGA();
+  });
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        ></script>
+        <script src="/utils/ga.js"></script>
+      </Head>
       <body className="dark">
         <Main />
         <NextScript />
