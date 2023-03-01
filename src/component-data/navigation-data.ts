cc34411a7d466ab8f6bb2f43132adedb43ad7ddc
Anthony Stolp <47982927+AJStolp@users.noreply.github.com>
2023-03-01 15:46:54 -0500

    removed cursor affect on mobile && fixed link visibility issue (#11)

--- src/component-data/navigation-data.ts
+++ src/component-data/navigation-data.ts
@@ -5,7 +5,7 @@ type Navigation = {
 export const NavigationData: { [key: string]: Navigation } = {
   parentSection: {
     className:
-      "flex flex-row flex-wrap justify-end gap-4 items-center group z-10",
+      "flex flex-row flex-wrap justify-end gap-4 items-center group z-10 bg-white dark:bg-black rounded",
   },
   button: {
     btnClassName: "",
