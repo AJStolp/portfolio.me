34c70eef0622a42833c89242c2cc54e9978210e9
Anthony Stolp <47982927+AJStolp@users.noreply.github.com>
2023-03-25 10:30:01 -0400

    fixed typo (#25)

--- src/component-data/hero-data.ts
+++ src/component-data/hero-data.ts
@@ -3,9 +3,9 @@ import { IHero } from "@/interfaces/IHero";
 const HeroData: IHero[] = [
   {
     id: 1,
-    heading: "My name is Anthony",
+    heading: " My name is Anthony",
     colorName: "Anthony,",
-    copy: "I am a Developer. If you're a business seeking a web presence or are looking to hire, contact me here. My portfolio showcases the websites that I have had the opportunity to colloborate on.",
+    copy: "I am a Developer. If you're a business seeking a web presence or are looking to hire, contact me here. My portfolio showcases the websites that I have had the opportunity to collaborate on.",
     image: "/assets/portfolio-logo.webp",
     alt: "Technology graphic",
     cta1: "Projects",
